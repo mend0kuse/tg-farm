2f1ac388254bd387d2703035b872710b4195fdf3
mend0kuse <vasilievsam03@mail.ru>
2024-10-02 01:20:07 +0800

    enable pixel worker

--- src/thread.ts
+++ src/thread.ts
@@ -3,11 +3,12 @@ import { TAccountData } from './scripts/accounts-generator';
 import { baseLogger } from './shared/logger';
 import { runCatsWorker } from './cats/worker';
 import { runHrumWorker } from './xrum/worker';
+import { runPixelWorker } from './pixel/worker';
 
 const user = workerData as TAccountData;
 
 (async () => {
     baseLogger.log(`Воркер ${user.index} Старт`);
 
-    await Promise.allSettled([runHrumWorker(user), runCatsWorker(user)]);
+    await Promise.allSettled([runHrumWorker(user), runPixelWorker(user), runCatsWorker(user)]);
 })();
