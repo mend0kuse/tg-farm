2b23177c8fa2896c048f125437568fb68ceb7b63
mend0kuse <vasilievsam03@mail.ru>
2024-10-03 01:41:09 +0800

    enable all workers

--- src/thread.ts
+++ src/thread.ts
@@ -4,11 +4,19 @@ import { baseLogger } from './shared/logger';
 import { runCatsWorker } from './cats/worker';
 import { runHrumWorker } from './xrum/worker';
 import { runPixelWorker } from './pixel/worker';
+import { runVanaWorker } from './vana/worker';
+import { runEmpireWorker } from './xempire/worker';
 
 const user = workerData as TAccountData;
 
 (async () => {
     baseLogger.log(`Воркер ${user.index} Старт`);
 
-    await Promise.allSettled([runHrumWorker(user), runPixelWorker(user), runCatsWorker(user)]);
+    await Promise.allSettled([
+        runHrumWorker(user),
+        runPixelWorker(user),
+        runEmpireWorker(user),
+        runCatsWorker(user),
+        runVanaWorker(user),
+    ]);
 })();
