5df22628665f9b1577adfa20bb78cb5f54211800
mend0kuse <vasilievsam03@mail.ru>
2024-10-03 23:32:41 +0800

    comment ref system empire

--- src/xempire/worker.ts
+++ src/xempire/worker.ts
@@ -1,8 +1,5 @@
 import { TelegramClient } from '@mtcute/node';
-import { APP_CONFIG } from '../config';
-import { REFERRAL_MAP_2 } from '../constants';
 import { TAccountData } from '../scripts/accounts-generator';
-import { excelUtility } from '../shared/excel/excel';
 import { baseLogger } from '../shared/logger';
 import { telegramApi } from '../shared/telegram/telegram-api';
 import { sleep, random } from '../shared/utils';
@@ -10,37 +7,35 @@ import { xEmpireDatabase } from './database';
 import { XEmpire } from './xempire';
 
 export const runEmpireWorker = async (user: TAccountData, telegramClient: TelegramClient) => {
-    const accounts = excelUtility.getAccounts();
-
     let cycle = 1;
 
     xEmpireDatabase.init();
 
-    const refererIndex = REFERRAL_MAP_2[user.index];
-    let refCode = `hero${APP_CONFIG.MASTER_USER_ID}`;
-    let isCreated = false;
+    // const refererIndex = REFERRAL_MAP_2[user.index];
+    const refCode = ``;
+    const isCreated = false;
 
-    while (true) {
-        const myAccount = await xEmpireDatabase.findByIndex(user.index);
-        if (myAccount) {
-            isCreated = true;
-            break;
-        }
+    // while (true) {
+    //     const myAccount = await xEmpireDatabase.findByIndex(user.index);
+    //     if (myAccount) {
+    //         isCreated = true;
+    //         break;
+    //     }
 
-        if (refererIndex === 1) {
-            break;
-        }
+    //     if (refererIndex === 1) {
+    //         break;
+    //     }
 
-        const refererAccount: any = await xEmpireDatabase.findByIndex(refererIndex);
-        if (refererAccount) {
-            const { id } = accounts.find((acc) => acc.index === refererIndex)!;
-            refCode = `hero${id}`;
-            break;
-        }
+    //     const refererAccount: any = await xEmpireDatabase.findByIndex(refererIndex);
+    //     if (refererAccount) {
+    //         const { id } = accounts.find((acc) => acc.index === refererIndex)!;
+    //         refCode = `hero${id}`;
+    //         break;
+    //     }
 
-        baseLogger.accentLog(`[XRUM_${user.index}] В базе не найден аккаунт referer. Задержка 5 минут...`);
-        await sleep(60 * 5);
-    }
+    //     baseLogger.accentLog(`[X_${user.index}] В базе не найден аккаунт referer. Задержка 5 минут...`);
+    //     await sleep(60 * 5);
+    // }
 
     while (cycle < 6) {
         try {
