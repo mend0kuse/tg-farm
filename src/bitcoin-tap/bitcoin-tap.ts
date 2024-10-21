00d3f5f15a35300ce337dcd8f3c95d7cd3f5d6a7
mend0kuse <vasilievsam03@mail.ru>
2024-10-21 18:31:49 +0800

    disable bitcoin notification

--- src/bitcoin-tap/bitcoin-tap.ts
+++ src/bitcoin-tap/bitcoin-tap.ts
@@ -250,13 +250,13 @@ export class BitcoinTap {
     async getRouletteStats() {
         try {
             const { stats } = (await this.api.get(`/roulette/event/stats/?user_id=${this.profile.id}`)).data;
-            for (const key in stats) {
-                if (key !== 'points') {
-                    await telegramApi.sendBotNotification(
-                        `[BITCOIN-TAP_${this.account.index}] Найден ключ ${key} в результат рулетки. Значение ${JSON.stringify(stats[key])}`
-                    );
-                }
-            }
+            // for (const key in stats) {
+            //     if (key !== 'points') {
+            //         await telegramApi.sendBotNotification(
+            //             `[BITCOIN-TAP_${this.account.index}] Найден ключ ${key} в результат рулетки. Значение ${JSON.stringify(stats[key])}`
+            //         );
+            //     }
+            // }
             this.logger.log('Успешно получена статистика рулетки');
         } catch (error) {
             this.logger.error('Ошибка получения статистики рулетки', this.handleError(error));
